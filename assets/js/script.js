8aa841effb488645c9672c70d22526a3840583fc
Loan <lgiovaniloh@gmail.com>
2025-06-03 17:04:32 +0100

    Changement musique

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,3 @@
-
-
-/*let n = Math.floor(Math.random() * 10);
-const input = document.querySelector(".input-field");
-const msg = document.getElementById("msg")
-const att = document.getElementById("attempts");
-let i = 3;
-
-    input.addEventListener("keydown" , (event) =>{
-        i--;
-        while (i > 0) {
-            if (event.key === "Enter") {
-                let num = parseInt(input.value);
-                if (isNaN(num) || num < 0 || num > 9) {
-                    msg.textContent = "Veuillez entrer un nombre entre 0 et 9.";
-                    input.value = "";
-                    return;
-                }
-
-                if (num === n) {
-                    msg.textContent = "Bravo ! Vous avez trouvé le nombre mystère : " + n + ".";
-                    input.disabled = true;
-                    return;
-                } else {
-                    msg.textContent = "Mauvais choix ! Le nombre mystère n'est pas " + num + ".";
-                    attcoun.textContent = i;
-                }
-                
-                input.value = "";
-                break;
-            }
-        }
-        if (i === 0) {
-            msg.textContent = "Désolé, vous avez épuisé vos tentatives. Le nombre mystère était " + num + ".";
-            input.disabled = true;
-        }
-
-    });*/
-
 function stargame() {
     let n = Math.floor(Math.random() * 10);
     const input = document.querySelector(".input-field");
@@ -50,13 +11,12 @@ function stargame() {
 
     guess.addEventListener("click", function (){  
         let num = parseInt(input.value);
-        // if (event.key === "Enter") {         
+                 
         if (isNaN(num) || num < 0 || num > 9) {
             msg.textContent = "Veuillez entrer un nombre entre 0 et 9.";
             input.value = "";
             return;
         }
-        //i--;
         if (num === n) {
             msg.textContent = "Bravo ! Vous avez trouvé le nombre mystère : " + n + ".";
             document.querySelector(".attempts-left").style.display = "none";
@@ -113,7 +73,7 @@ function stargame() {
     })
 
 }
-const musique = new Audio("assets/ressources/calmly-30-sec-edit-301145.mp3");
+const musique = new Audio("assets/ressources/dark-game-beat-main-track-344552.mp3");
 musique.load();
 musique.loop = true;
 musique.volume = 0.2;
